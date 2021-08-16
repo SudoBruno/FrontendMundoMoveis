eba49466bc953530f846fc3d455fae3859da1b7e
SudoBruno <brunoleonardo.profissional@gmail.com>
2021-08-16 08:58:14 -0300

    visualização de relatórios

--- src/pages/Warehouse/Consult/Exit/index.tsx
+++ src/pages/Warehouse/Consult/Exit/index.tsx
@@ -1,6 +1,10 @@
 import React, { FormEvent, useState } from 'react';
-import { DownloadOutlined, SearchOutlined } from '@ant-design/icons';
-import { Button, Col, Input, Layout, Row, Space, Table } from 'antd';
+import {
+  DownloadOutlined,
+  SearchOutlined,
+  EyeOutlined,
+} from '@ant-design/icons';
+import { Button, Col, Input, Layout, Modal, Row, Space, Table } from 'antd';
 import Highlighter from 'react-highlight-words';
 import styles from '../../../../styles/app.module.scss';
 import { CSVLink } from 'react-csv';
@@ -28,19 +32,21 @@ interface IProps {
 export default function categories({ exit }: IProps) {
   const [csvData, setCsvData] = useState([]);
   const [exits, setexits] = useState(exit);
+  const [isOpenModal, setIsModalOpen] = useState(false);
   const [headers, setHeaders] = useState([
     { label: 'USUÁRIO', key: 'users_name' },
     { label: 'INSUMO', key: 'raw_material_name' },
-    { label: 'POSIÇÃO', key: 'description' },
+    { label: 'POSIÇÃO', key: 'position_name' },
     { label: 'QUANTIDADE', key: 'quantity' },
     { label: 'CRIADO EM', key: 'created_at' },
   ]);
 
-  async function handleClickCsvLink(receipt_id) {
-    const response = await api.get(`/warehouse/exit/${receipt_id}`);
+  async function handleClickCsvLink(exit_id: string) {
+    const response = await api.get(`/warehouse/exit/${exit_id}`);
 
     setCsvData(response.data);
   }
+
   class SearchTable extends React.Component {
     state = {
       searchText: '',
@@ -166,46 +172,21 @@ export default function categories({ exit }: IProps) {
           render: (record) => {
             return (
               <>
-                {csvData.length < 1 && (
-                  <Button
-                    className={styles.button}
-                    style={{
-                      borderRadius: '26px',
-                      marginTop: '10px',
-                      marginLeft: '10px',
-                      backgroundColor: '#1c3030',
-                    }}
-                    onClick={() => {
-                      handleClickCsvLink(record.id);
-                    }}
-                  >
-                    <DownloadOutlined style={{ fontSize: '16px' }} />
-                  </Button>
-                )}
-
-                {csvData.length > 0 && (
-                  <Button
-                    className={styles.button}
-                    style={{
-                      borderRadius: '26px',
-                      marginTop: '10px',
-                      marginLeft: '10px',
-                      backgroundColor: '#2F4F4F',
-                    }}
-                    onClick={() => {
-                      handleClickCsvLink(record.id);
-                    }}
-                  >
-                    <CSVLink
-                      data={csvData}
-                      style={{ color: 'white' }}
-                      filename={`Saída-${format(new Date(), 'dd-MM-yyyy')}.csv`}
-                      headers={headers}
-                    >
-                      <DownloadOutlined style={{ fontSize: '16px' }} />
-                    </CSVLink>
-                  </Button>
-                )}
+                <Button
+                  className={styles.button}
+                  style={{
+                    borderRadius: '26px',
+                    marginTop: '10px',
+                    marginLeft: '10px',
+                    backgroundColor: '#1c3030',
+                  }}
+                  onClick={() => {
+                    setIsModalOpen(true);
+                    handleClickCsvLink(record.id);
+                  }}
+                >
+                  <EyeOutlined />
+                </Button>
               </>
             );
           },
@@ -219,11 +200,210 @@ export default function categories({ exit }: IProps) {
     }
   }
 
+  class SearchTableForModal extends React.Component {
+    state = {
+      searchText: '',
+      searchedColumn: '',
+    };
+    searchInput: Input;
+    getColumnSearchProps = (dataIndex) => ({
+      filterDropdown: ({
+        setSelectedKeys,
+        selectedKeys,
+        confirm,
+        clearFilters,
+      }) => (
+        <div style={{ padding: 8 }}>
+          <Input
+            ref={(node) => {
+              this.searchInput = node;
+            }}
+            placeholder={`Search ${dataIndex}`}
+            value={selectedKeys[0]}
+            onChange={(e) =>
+              setSelectedKeys(e.target.value ? [e.target.value] : [])
+            }
+            onPressEnter={() =>
+              this.handleSearch(selectedKeys, confirm, dataIndex)
+            }
+            style={{ marginBottom: 8, display: 'block' }}
+          />
+          <Space>
+            <Button
+              type="primary"
+              onClick={() =>
+                this.handleSearch(selectedKeys, confirm, dataIndex)
+              }
+              icon={<SearchOutlined />}
+              size="small"
+              style={{ width: 90 }}
+            >
+              Buscar
+            </Button>
+            <Button
+              onClick={() => this.handleReset(clearFilters)}
+              size="small"
+              style={{ width: 90 }}
+            >
+              Limpar
+            </Button>
+          </Space>
+        </div>
+      ),
+      filterIcon: (filtered) => (
+        <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
+      ),
+      onFilter: (value, record) =>
+        record[dataIndex]
+          ? record[dataIndex]
+              .toString()
+              .toLowerCase()
+              .includes(value.toLowerCase())
+          : '',
+      onFilterDropdownVisibleChange: (visible) => {
+        if (visible) {
+          setTimeout(() => this.searchInput.select(), 100);
+        }
+      },
+      render: (text) =>
+        this.state.searchedColumn === dataIndex ? (
+          <Highlighter
+            highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+            searchWords={[this.state.searchText]}
+            autoEscape
+            textToHighlight={text ? text.toString() : ''}
+          />
+        ) : (
+          text
+        ),
+    });
+
+    handleSearch = (selectedKeys, confirm, dataIndex) => {
+      confirm();
+      this.setState({
+        searchText: selectedKeys[0],
+        searchedColumn: dataIndex,
+      });
+    };
+
+    handleReset = (clearFilters) => {
+      clearFilters();
+      this.setState({ searchText: '' });
+    };
+
+    render() {
+      const columns = [
+        {
+          title: 'Usuário',
+          dataIndex: 'users_name',
+          key: 'users_name',
+          width: '40%',
+          ...this.getColumnSearchProps('users_name'),
+          sorter: (a, b) => a.users_name.length - b.users_name.length,
+        },
+
+        {
+          title: 'Lote',
+          dataIndex: 'raw_stock_cargo',
+          key: 'raw_stock_cargo',
+          width: '40%',
+          ...this.getColumnSearchProps('raw_stock_cargo'),
+          sorter: (a, b) => a.raw_stock_cargo.length - b.raw_stock_cargo.length,
+        },
+        {
+          title: 'Insumo',
+          dataIndex: 'raw_material_name',
+          key: 'raw_material_name',
+          width: '40%',
+          ...this.getColumnSearchProps('raw_material_name'),
+          sorter: (a, b) =>
+            a.raw_material_name.length - b.raw_material_name.length,
+        },
+        {
+          title: 'Quantidade',
+          dataIndex: 'quantity',
+          key: 'quantity',
+          width: '40%',
+          ...this.getColumnSearchProps('quantity'),
+          sorter: (a, b) => a.quantity.length - b.quantity.length,
+        },
+        {
+          title: 'Posição',
+          dataIndex: 'position_name',
+          key: 'position_name',
+          width: '40%',
+          ...this.getColumnSearchProps('position_name'),
+          sorter: (a, b) => a.position_name.length - b.position_name.length,
+        },
+        {
+          title: 'Criado Em',
+          dataIndex: 'created_at',
+          key: 'created_at',
+          width: '40%',
+          ...this.getColumnSearchProps('created_at'),
+          sorter: (a, b) => a.created_at.length - b.created_at.length,
+        },
+      ];
+      return (
+        <>
+          <Table columns={columns} dataSource={csvData} />
+        </>
+      );
+    }
+  }
+
   return (
-    <Layout>
-      <Row justify="end"></Row>
-      <SearchTable />
-    </Layout>
+    <>
+      <Layout>
+        <SearchTable />
+      </Layout>
+      <Modal
+        width={900}
+        title="Alterar Estoque"
+        visible={isOpenModal}
+        onCancel={() => setIsModalOpen(false)}
+        footer={[
+          <Button
+            key="back"
+            type="default"
+            onClick={() => {
+              setIsModalOpen(false);
+            }}
+          >
+            Cancelar
+          </Button>,
+        ]}
+      >
+        <Row justify="end">
+          <Col>
+            {csvData.length > 0 && (
+              <Button
+                className={styles.button}
+                style={{
+                  borderRadius: '26px',
+                  marginTop: '10px',
+                  marginLeft: '10px',
+                  backgroundColor: '#2F4F4F',
+                }}
+              >
+                <CSVLink
+                  data={csvData}
+                  style={{ color: 'white' }}
+                  filename={`Saída-${format(new Date(), 'dd-MM-yyyy')}.csv`}
+                  headers={headers}
+                >
+                  <DownloadOutlined
+                    style={{ fontSize: '16px', marginRight: '5px' }}
+                  />
+                  Baixar Relatório
+                </CSVLink>
+              </Button>
+            )}
+          </Col>
+        </Row>
+        <SearchTableForModal />
+      </Modal>
+    </>
   );
 }
 
--- src/pages/Warehouse/Consult/Receipt/index.tsx
+++ src/pages/Warehouse/Consult/Receipt/index.tsx
@@ -1,6 +1,10 @@
 import React, { FormEvent, useState } from 'react';
-import { DownloadOutlined, SearchOutlined } from '@ant-design/icons';
-import { Button, Col, Input, Layout, Row, Space, Table } from 'antd';
+import {
+  DownloadOutlined,
+  SearchOutlined,
+  EyeOutlined,
+} from '@ant-design/icons';
+import { Button, Col, Input, Layout, Modal, Row, Space, Table } from 'antd';
 import Highlighter from 'react-highlight-words';
 import styles from '../../../../styles/app.module.scss';
 import { CSVLink } from 'react-csv';
@@ -10,6 +14,7 @@ import { getAPIClient } from '../../../../services/axios';
 import { format } from 'date-fns';
 import { pt } from 'date-fns/locale';
 import { api } from '../../../../services/api';
+import formatRelativeWithOptions from 'date-fns/fp/formatRelativeWithOptions';
 
 interface IReceipt {
   id: string;
@@ -26,18 +31,24 @@ interface IProps {
 
 export default function categories({ receipt }: IProps) {
   const [csvData, setCsvData] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [receipts, setReceipts] = useState(receipt);
   const [headers, setHeaders] = useState([
     { label: 'Nome da Entrada', key: 'warehouse_receipt_description' },
     { label: 'Insumo', key: 'warehouse_raw_material_name' },
     { label: 'Quantidade', key: 'Quantity' },
+    { label: 'Valor Total', key: 'grade_value' },
     { label: 'Criado Em', key: 'created_at' },
   ]);
 
   async function handleClickCsvLink(receipt_id) {
+    console.log(receipt_id);
+
     const response = await api.get('/warehouse/receipt/raw-material', {
-      params: { receipt_id: receipt_id },
+      params: { id: receipt_id },
     });
+
+    console.log(response.data);
 
     setCsvData(response.data);
   }
@@ -173,49 +184,21 @@ export default function categories({ receipt }: IProps) {
           render: (record) => {
             return (
               <>
-                {csvData.length < 1 && (
-                  <Button
-                    className={styles.button}
-                    style={{
-                      borderRadius: '26px',
-                      marginTop: '10px',
-                      marginLeft: '10px',
-                      backgroundColor: '#1c3030',
-                    }}
-                    onClick={() => {
-                      handleClickCsvLink(record.id);
-                    }}
-                  >
-                    <DownloadOutlined style={{ fontSize: '16px' }} />
-                  </Button>
-                )}
-
-                {csvData.length > 0 && (
-                  <Button
-                    className={styles.button}
-                    style={{
-                      borderRadius: '26px',
-                      marginTop: '10px',
-                      marginLeft: '10px',
-                      backgroundColor: '#2F4F4F',
-                    }}
-                    onClick={() => {
-                      handleClickCsvLink(record.id);
-                    }}
-                  >
-                    <CSVLink
-                      data={csvData}
-                      style={{ color: 'white' }}
-                      filename={`Entrada-${format(
-                        new Date(),
-                        'dd-MM-yyyy'
-                      )}.csv`}
-                      headers={headers}
-                    >
-                      <DownloadOutlined style={{ fontSize: '16px' }} />
-                    </CSVLink>
-                  </Button>
-                )}
+                <Button
+                  className={styles.button}
+                  style={{
+                    borderRadius: '26px',
+                    marginTop: '10px',
+                    marginLeft: '10px',
+                    backgroundColor: '#1c3030',
+                  }}
+                  onClick={() => {
+                    setIsModalOpen(true);
+                    handleClickCsvLink(record.id);
+                  }}
+                >
+                  <EyeOutlined />
+                </Button>
               </>
             );
           },
@@ -229,12 +212,214 @@ export default function categories({ receipt }: IProps) {
     }
   }
 
+  class SearchTableModal extends React.Component {
+    state = {
+      searchText: '',
+      searchedColumn: '',
+    };
+    searchInput: Input;
+    getColumnSearchProps = (dataIndex) => ({
+      filterDropdown: ({
+        setSelectedKeys,
+        selectedKeys,
+        confirm,
+        clearFilters,
+      }) => (
+        <div style={{ padding: 8 }}>
+          <Input
+            ref={(node) => {
+              this.searchInput = node;
+            }}
+            placeholder={`Search ${dataIndex}`}
+            value={selectedKeys[0]}
+            onChange={(e) =>
+              setSelectedKeys(e.target.value ? [e.target.value] : [])
+            }
+            onPressEnter={() =>
+              this.handleSearch(selectedKeys, confirm, dataIndex)
+            }
+            style={{ marginBottom: 8, display: 'block' }}
+          />
+          <Space>
+            <Button
+              type="primary"
+              onClick={() =>
+                this.handleSearch(selectedKeys, confirm, dataIndex)
+              }
+              icon={<SearchOutlined />}
+              size="small"
+              style={{ width: 90 }}
+            >
+              Buscar
+            </Button>
+            <Button
+              onClick={() => this.handleReset(clearFilters)}
+              size="small"
+              style={{ width: 90 }}
+            >
+              Limpar
+            </Button>
+          </Space>
+        </div>
+      ),
+      filterIcon: (filtered) => (
+        <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
+      ),
+      onFilter: (value, record) =>
+        record[dataIndex]
+          ? record[dataIndex]
+              .toString()
+              .toLowerCase()
+              .includes(value.toLowerCase())
+          : '',
+      onFilterDropdownVisibleChange: (visible) => {
+        if (visible) {
+          setTimeout(() => this.searchInput.select(), 100);
+        }
+      },
+      render: (text) =>
+        this.state.searchedColumn === dataIndex ? (
+          <Highlighter
+            highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+            searchWords={[this.state.searchText]}
+            autoEscape
+            textToHighlight={text ? text.toString() : ''}
+          />
+        ) : (
+          text
+        ),
+    });
+
+    handleSearch = (selectedKeys, confirm, dataIndex) => {
+      confirm();
+      this.setState({
+        searchText: selectedKeys[0],
+        searchedColumn: dataIndex,
+      });
+    };
+
+    handleReset = (clearFilters) => {
+      clearFilters();
+      this.setState({ searchText: '' });
+    };
+
+    render() {
+      const columns = [
+        {
+          title: 'Lote',
+          dataIndex: 'warehouse_receipt_description',
+          key: 'warehouse_receipt_description',
+          width: '30%',
+          ...this.getColumnSearchProps('warehouse_receipt_description'),
+          sorter: (a, b) =>
+            a.warehouse_receipt_description.length -
+            b.warehouse_receipt_description.length,
+        },
+        {
+          title: 'Insumo',
+          dataIndex: 'warehouse_raw_material_name',
+          key: 'warehouse_raw_material_name',
+          width: '20%',
+          ...this.getColumnSearchProps('warehouse_raw_material_name'),
+          sorter: (a, b) =>
+            a.warehouse_raw_material_name.length -
+            b.warehouse_raw_material_name.length,
+        },
+        {
+          title: 'Quantidade',
+          dataIndex: 'quantity',
+          key: 'quantity',
+          width: '20%',
+          ...this.getColumnSearchProps('quantity'),
+          sorter: (a, b) => a.quantity.length - b.quantity.length,
+        },
+        {
+          title: 'Valor Total',
+          dataIndex: 'grade_value',
+          key: 'grade_value',
+          width: '20%',
+          ...this.getColumnSearchProps('grade_value'),
+          sorter: (a, b) => a.grade_value.length - b.grade_value.length,
+        },
+        {
+          title: 'Criado Em',
+          dataIndex: 'created_at',
+          key: 'created_at',
+          width: '40%',
+          ...this.getColumnSearchProps('created_at'),
+          sorter: (a, b) => a.created_at.length - b.created_at.length,
+        },
+      ];
+      return (
+        <>
+          <Table columns={columns} dataSource={csvData} />
+        </>
+      );
+    }
+  }
+
   return (
     <Layout>
-      <Row justify="end">
-        <Col></Col>
-      </Row>
+      <Modal
+        width={900}
+        title="Alterar Estoque"
+        visible={isModalOpen}
+        onCancel={() => setIsModalOpen(false)}
+        footer={[
+          <Button
+            key="back"
+            type="default"
+            onClick={() => {
+              setIsModalOpen(false);
+            }}
+          >
+            Cancelar
+          </Button>,
+        ]}
+      >
+        <Row justify="end">
+          <Col>
+            {csvData.length < 1 && (
+              <Button
+                className={styles.button}
+                style={{
+                  borderRadius: '26px',
+                  marginTop: '10px',
+                  marginLeft: '10px',
+                  backgroundColor: '#1c3030',
+                }}
+              >
+                <DownloadOutlined style={{ fontSize: '16px' }} />
+              </Button>
+            )}
 
+            {csvData.length > 0 && (
+              <Button
+                className={styles.button}
+                style={{
+                  borderRadius: '26px',
+                  marginTop: '10px',
+                  marginLeft: '10px',
+                  backgroundColor: '#2F4F4F',
+                }}
+              >
+                <CSVLink
+                  data={csvData}
+                  style={{ color: 'white' }}
+                  filename={`Entrada-${format(new Date(), 'dd-MM-yyyy')}.csv`}
+                  headers={headers}
+                >
+                  <DownloadOutlined
+                    style={{ fontSize: '16px', marginRight: '5px' }}
+                  />
+                  Baixar Relatório
+                </CSVLink>
+              </Button>
+            )}
+          </Col>
+        </Row>
+        <SearchTableModal />
+      </Modal>
       <SearchTable />
     </Layout>
   );
